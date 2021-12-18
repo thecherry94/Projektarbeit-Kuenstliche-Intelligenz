86d0933082ea4fc30c97a4c143e382eeb6b6604e
thecherry94 <philipp.kirsch94@googlemail.com>
2021-12-18 22:30:22 +0100

    Update DataAugmentation.py
    Added functionality to analyzeDirectory method

--- Data-Augmentation/DataAugmentation.py
+++ Data-Augmentation/DataAugmentation.py
@@ -1,4 +1,6 @@
 import os
+import pathlib as pl
+from PIL import Image 
 
 class DataAugmentation:
     """
@@ -9,68 +11,71 @@ class DataAugmentation:
         'position': {
             'translation': {
                 'active': True,
-                'iterations': 100,
+                'iterations': 50,
             },
             'rotation': {
                 'active': True,
-                'iterations': 180
+                'iterations': 90
             },
             'cropping': {
                 'active': True,
-                'iterations': 25
+                'iterations': 50
             },
             'flipping': {
                 'active': True
             },
             'scaling': {
                 'active': True, 
-                'iterations': 25,
+                'iterations': 50,
                 'min': 0.25,
                 'max': 3,
             },
             'shearing': {
                 'active': True, 
-                'iterations': 25,
+                'iterations': 50,
                 'maxAngle': 3,
             }             
         },
         'color': {
             'brightness': {
                 'active': True,
-                'iterations': 25,
+                'iterations': 50,
                 'min': 0.2,
                 'max': 2
             },
             'contrast': {
                 'active': True,
-                'iterations': 25
+                'iterations': 50
             },
             'saturation': {
                 'active': True,
-                'iterations': 25
+                'iterations': 50
             },
             'hue': {
                 'active': True,
-                'iterations': 25
+                'iterations': 50
             }
         },
         'noise': {
             'gaussian': {
                 'active': True,
-                'iterations': 25
+                'iterations': 50
             },
             'saltpepper': {
-                'active': True
+                'active': True,
+                'iterations': 50
             },
             'speckle': {
-                'active': True
+                'active': True,
+                'iterations': 50
             }
         },
         'grayscale': True,
         'repeatForGrayscale': True
     }
 
-    def __init__(self) -> None: 
+    def __init__(self, rescale_size=256) -> None: 
+        self.rescale_size = rescale_size
         pass
 
     def analyzeDirectory(self, path, only_images=True):
@@ -84,6 +89,20 @@ class DataAugmentation:
             - dimensions (width/height tuple)
             - file size (in kB)
         """
+
+        info = []
+        for entry in os.scandir(path):
+            if entry.is_file:
+                if entry.name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+                    info.append({
+                        'filename': entry.name,
+                        'type': pl.Path(entry).suffix[1:],
+                        'dimensions': Image.open(os.path.abspath(path + '\\' + entry.name)).size,
+                        'size': entry.stat().st_size / 1024
+                    })
+
+        return info
+
         pass
 
     def processImageBatch(self, batch, options=defaultImageProcessingOptions):
@@ -110,7 +129,7 @@ class DataAugmentation:
     def process(self, path='..\\images', options={'imageProcessing': defaultImageProcessingOptions}):
         """
         Processes all images inside the given directory. 
-        Function expects to find each different images of each class to classify in its own subdirectory and will generate new subdirectories for the new images
+        Function expects to find different images of each class to classify in its own subdirectory and will generate new subdirectories for the new images
         """
         pass
     pass
