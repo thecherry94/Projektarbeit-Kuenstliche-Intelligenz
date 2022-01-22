a11ced2d9f4a736287cc6c23a332184fe953b367
k4t-hs <96535728+k4t-hs@users.noreply.github.com>
2022-01-22 17:14:39 +0100

    naive bayes adapted

--- modules/dataframe_management.py
+++ modules/dataframe_management.py
@@ -9,16 +9,12 @@ def features_separated_by_class(df):
     
     for i in range(len(df.index)):
         row = df.iloc[i,:]
-        c = row.iloc[3]
-        row = row.iloc[4:]
+        c = row.iloc[-1]
+        row = row.iloc[:-1]
         if c not in separated_dfs:
             separated_dfs[c] = pd.DataFrame()
         separated_dfs[c] = separated_dfs[c].append(row, ignore_index=True)
     
-    # for _, d in separated_dfs.items():
-    #     print(get_mean_std_num(d)[2])
-    #     print("------------------------------------------")
-
     return separated_dfs
 
 def get_mean_std_num(df):
@@ -31,11 +27,6 @@ def get_mean_std_num_per_class(df):
         mean_std_nums[c] = get_mean_std_num(row)
         
     return mean_std_nums
-        
-        
-# df = pd.read_csv(os.path.join(".", "data", "features.csv"), sep=';')#, usecols=[3,4,5,6,7])
-# # print(df)
-# msn = get_mean_std_num_per_class(df)
-# # print(msn)
+
 
         
--- modules/naive_bayes.py
+++ modules/naive_bayes.py
@@ -16,68 +16,16 @@ def gaussian_probability(x, mean, std):
 
 def class_probabilities(row, mean_std_nums):
     # get the number of all training data elements for all classes
-    num_testdata = sum([mean_std_nums[c][0][2] for c in mean_std_nums])
+    num_testdata = sum([mean_std_nums[c][2] for c in mean_std_nums])
 
     probabilities = dict()
     for c, mean_std_num in mean_std_nums.items():
         # calculate the a priori probability
-        prior = mean_std_nums[c][0][2]/float(num_testdata)
+        prior = mean_std_nums[c][2]/float(num_testdata)
         probabilities[c] = prior
         mean, std, _= mean_std_num
         likelihood = gaussian_probability(row, mean, std)
         for p in likelihood:
-            probabilities[c] *= p
-        # evidence += probabilities[c]    
+            probabilities[c] *= p    
     return probabilities
 
-
-classes = {0 : "bottle opener",
-           1 : "can opener",
-           2 : "corc screw",
-           3 : "multi tool"}
-
-
-parent_dir = os.getcwd()
-wd = os.getcwd().split(os.sep)
-data_dir = wd[0] + os.sep
-for folder in wd[1:]:
-    data_dir = os.path.join(data_dir, folder)
-    if folder == "ai-assignment":
-        break
-data = os.path.join(data_dir, "data", "features.csv")
-# os.path.join(".", "data", "features.csv")
-
-df = pd.read_csv(data, sep=';')#, usecols=[3,4,5,6,7])
-# df = df.iloc[:-10, :]
-df_test = dm.features_separated_by_class(df)#df.iloc[570:580, 4:]
-# df_test = df_test[3]
-# print(df)
-# print(df.iloc[:-1, :])
-# print(df.iloc[-1, :])
-# print(df)
-msn = dm.get_mean_std_num_per_class(df)
-# print(df_test.apply(class_probabilities, axis=1, args=(msn)))
-# correct_ratio = 0
-# num = len(df_test.index)
-
-for i in range(15):
-    rdm_class = randrange(0, 4)
-    df_class = df_test[rdm_class]
-    rdm_img_idx = randrange(0, len(df_class.index))
-    probs = class_probabilities(df_class.iloc[rdm_img_idx, :], msn)
-    prediction = classes[max(probs, key=probs.get)]
-    print("Predicted:", prediction, "\t\tTarget:", classes[rdm_class], "\t\tCorrect?:", prediction == classes[rdm_class])
-
-
-# for _, test in df_test.iterrows():
-#     probs = class_probabilities(test, msn)#df.iloc[-1, 4:])
-#     # print(probs)#pd.Series([0.5,2,3,4], index=df.columns[4:])))
-#     # print(max(probs.values()))#.values()))
-#     # print(max(probs, key=probs.get))
-#     max_idx = max(probs, key=probs.get)
-#     # if max_idx == 3:
-#     #     correct_ratio += 1
-#     print(classes[max_idx])
-    
-# print(correct_ratio/num)
-# print(msn)
